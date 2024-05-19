20d6964337318ae38852c92564e2ff95730df6f5
shivambhagat79 <shivambhagat2279+gitub@gmail.com>
2024-05-19 22:08:14 +0530

    created experience section template.

--- src/components/sections/Experience/experience.tsx
+++ src/components/sections/Experience/experience.tsx
@@ -1,10 +1,14 @@
+import { HoverEffect } from "@/components/ui/experience-card-group";
 import Title from "@/components/ui/title";
+import { experiences } from "../../../../public/data/experiences/experience";
 
 export default function Experience() {
   return (
     <div className="border-b-2 border-black">
       <Title title="My Experiences" />
-      <div className="h-60"></div>
+      <div className="px-20">
+        <HoverEffect items={experiences} />
+      </div>
     </div>
   );
 }

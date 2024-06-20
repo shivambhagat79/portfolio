72f24da50d50c3a866daa8028e5d13e0118b38bd
shivambhagat79 <shivambhagat2279+gitub@gmail.com>
2024-06-20 20:02:20 +0530

    refactor: Update Header component styles for responsiveness

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -19,7 +19,7 @@ const major_mono = Major_Mono_Display({
 
 export default function Header() {
   return (
-    <div className="h-screen w-screen text-lg border-b-2 border-black">
+    <div className="h-screen w-screen text-sm md:text-base xl:text-lg border-b-2 border-black">
       <Navbar />
       <div className="w-full absolute inset-0 h-screen -z-10">
         <SparklesCore
@@ -33,22 +33,25 @@ export default function Header() {
           particleColor="#99ccc1"
         />
       </div>
-      <div className="w-full h-full px-60 flex justify-center items-center">
+      <div className="w-full h-full px-5 md:px-24 xl:px-60 flex justify-center items-center">
         <div className="w-full">
-          <div className="w-full pb-4 flex border-b-2 border-black justify-between">
-            <div className="border-2 border-black px-4 py-2">
+          <div className="w-full pb-4 flex flex-col md:flex-row border-b-2 border-black items-end md:justify-between">
+            <div className="border-2 border-black px-4 py-2 mb-5 inline-block md:mb-0">
               Based in Punjab{"  "}
               <ArrowRight />
               {"  "}
               India
             </div>
-            <div className="bg-black text-white px-4 py-2">
+            <div></div>
+            <div className="bg-black text-white px-4 py-2 inline-block mb-5 md:mb-0">
               Status <ArrowRight />
               {"  "}
               <span>Open to Work</span>
             </div>
           </div>
-          <div className={`${major_mono.className} text-8xl mt-9 mb-24`}>
+          <div
+            className={`${major_mono.className} text-4xl md:text-6xl xl:text-8xl mt-9 mb-10 md:mb-16 xl:mb-24`}
+          >
             <TextGenerateEffect words="Hi, This Is Shivam" />
           </div>
           <div className="mb-5 bg-black inline-block text-white px-4 py-2">
@@ -68,7 +71,7 @@ export default function Header() {
       <div className="bottom-10 absolute left-1/2">
         <FontAwesomeIcon
           icon={faAngleDoubleDown}
-          className="text-teal-700/80 text-5xl"
+          className="text-teal-700/80 text-2xl md:text-3xl xl:text-5xl"
         />
       </div>
     </div>

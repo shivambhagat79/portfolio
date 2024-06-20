ebacc390bbf30494893d8e9a31a854e0cefae918
shivambhagat79 <shivambhagat2279+gitub@gmail.com>
2024-06-20 23:02:47 +0530

    refactor: Improve responsiveness of Navbar component

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -31,7 +31,12 @@ export default function Navbar() {
 
   return (
     <>
-      <nav className="w-screen border-b-2 border-black px-5 lg:px-30 xl:px-60 py-2 z-50">
+      <nav
+        className={cn(
+          "w-screen border-b-2 border-black px-5 lg:px-30 xl:px-60 py-2 z-50 transition-all duration-500",
+          navOpen ? " backdrop-blur-2xl" : ""
+        )}
+      >
         <div className="flex justify-between items-center">
           <div
             className={`${major_mono.className} font-bold text-2xl md:text-4xl bg-black text-white px-4 py-2`}
@@ -57,8 +62,8 @@ export default function Navbar() {
       </nav>
       <div
         className={cn(
-          "h-0 w-screen transition-all duration-500 overflow-hidden",
-          navOpen ? " h-screen" : ""
+          "h-0 w-screen transition-all duration-500 overflow-hidden absolute",
+          navOpen ? " h-screen backdrop-blur-2xl" : ""
         )}
       >
         <div className="flex flex-col items-center justify-center h-full">
